879bd468a69fea27216e50efac9f4293a85efa0d
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2023-10-03 15:48:59 +0200

    Create LagrangApprox.m

---
+++ Codes/LagrangApprox.m
@@ -0,0 +1,23 @@
+%---------------------------------------------------------------------
+function P = LagrangeApprox( yd, PSI, beq, Aeq, Ayg, byg, N )
+%---------------------------------------------------------------------
+% Computes analitically the weighting coefficients for the
+% approximation of 'yd' with RBF basis functions
+
+    L  = length( yd );
+    W  = eye( L ) .* 1e0;
+    Bi = vertcat( Aeq, Ayg );
+    Ri = vertcat( beq, byg );
+    Ba = PSI;
+    Ra = yd;
+
+    Ba2 = Ba' * W * Ba + eye(N).*realmin;
+    BRa = Ba' * W * Ra;
+
+    [ k, ~ ] = size( Bi );
+    Q = ( Bi / Ba2 ) * Bi'  + eye(k).*realmin ;
+
+    l = Q \ ( ( Bi / Ba2 ) * BRa - Ri );
+    P = ( Ba2 \ BRa ) - ( Ba2 \ Bi' ) * l ;
+
+end
