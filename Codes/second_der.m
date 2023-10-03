fcf276ede702f9c8cfd9460ad5ba3646b3fe5f79
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2023-10-03 15:50:26 +0200

    Create second_der.m

---
+++ Codes/second_der.m
@@ -0,0 +1,16 @@
+%---------------------------------------------------------------------
+function ddphi = second_der( rbf, drbf, ddrbf, sum_rbf, dsum_rbf, ddsum_rbf )
+%---------------------------------------------------------------------
+% Calculates analitically the second derivative of normalized RBF
+
+
+    a = @(s) ddrbf(s) .* sum_rbf(s) - rbf(s) .* ddsum_rbf(s);
+    b = @(s) drbf(s) .* sum_rbf(s) - rbf(s) .* dsum_rbf(s);
+
+
+    num = @(s) a(s) .* ( sum_rbf(s).^2 ) -  b(s) .* ( 2 * sum_rbf(s) ) .* dsum_rbf(s);
+    den = @(s) ( sum_rbf(s).^4 );
+
+    ddphi = @(s) num(s) ./ den(s);
+
+end
